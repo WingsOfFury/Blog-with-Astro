a5a78996bcdff2314735bb4b7008a1329e67ed88
WingsOfFury <akcengiz@gmx.de>
2023-11-21 12:56:07 +0100

    Begrüßungstext auf der Main Seite wurde aktualisiert

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,16 +1,16 @@
 import { useState } from 'preact/hooks';
 
-export default function Greeting({messages}) {
+export default function Greeting({ messages }) {
 
   const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
-  
+
   const [greeting, setGreeting] = useState(messages[0]);
 
   return (
-    <div> 
-      <h3>{greeting}! Thank you for visiting!</h3>
+    <div>
+      <h3>{greeting}! Schön das du hier bist!</h3>
       <button onClick={() => setGreeting(randomMessage())}>
-        New Greeting
+        Klick mich
       </button>
     </div>
   );
